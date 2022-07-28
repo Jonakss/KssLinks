9740cdae09798a02e94fe719e5407c5455b48487
Jonakss <jona.correapc@gmail.com>
2022-07-28 00:14:42 -0300

    Added api router in express server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
 import express from 'express'
 import { newLink } from './types'
 import * as linksService from './service/linksService'
-
+import apiRouter from './routes/api'
 
 const PORT:string = process.env.PORT || '3000'
 
 const app = express()
 app.use(express.json())
 
+app.use('/api', apiRouter)
+
 app.get('/', (_req, res) =>{
     res.send('Hello world')    
-})
-
-app.get('/new', (_req, res) =>{
-    const newlink: newLink = {
-        "text": "Prueba",
-        "url": "prueba.p"
-    }
-
-    const link =linksService.addLink(newlink)
-
-    res.send(`Se añadió el link ${link._id}.`)    
 })
 
 app.get('*', (_req, res) => {
