6534564540a9a4306e5123ae84584098d0ae6d90
Jonakss <jona.correapc@gmail.com>
2022-08-27 03:19:19 -0300

    update(ui): Add boostrap and basic design

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 //import { newLink } from './types'
-//import * as linksService from './service/linksService'
+import * as linksService from './service/linksService'
 import apiRouter from './routes/api'
 
 const PORT:string = process.env.PORT || '3000'
@@ -10,14 +10,13 @@ const app = express()
 //app.set('views', './views')
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
-
+app.use(express.static(__dirname + '/assets'))
 app.use(express.json())
 
 app.use('/api', apiRouter)
 
-app.get('/', (_req, res) =>{
-    //res.send(linksService.getLinks())    
-    res.render('index', { name:'Jonathan' })
+app.get('/', (_req, res) =>{    
+    res.render('index', { name:'Jonathan Correa Paiva', links: linksService.getLinks() })
 })
 
 app.get('*', (_req, res) => {
