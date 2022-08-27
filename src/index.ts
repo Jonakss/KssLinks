3962fadab7597cb973cc124bd38c2b2973a81548
Jonakss <jona.correapc@gmail.com>
2022-08-27 00:04:00 -0300

    update: Add initial pug integration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import express from 'express'
-import { newLink } from './types'
-import * as linksService from './service/linksService'
+//import { newLink } from './types'
+//import * as linksService from './service/linksService'
 import apiRouter from './routes/api'
 
 const PORT:string = process.env.PORT || '3000'
 
 const app = express()
+
+//app.set('views', './views')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug')
+
 app.use(express.json())
 
 app.use('/api', apiRouter)
 
 app.get('/', (_req, res) =>{
-    res.send('Hello world')    
+    //res.send(linksService.getLinks())    
+    res.render('index', { name:'Jonathan' })
 })
 
 app.get('*', (_req, res) => {
